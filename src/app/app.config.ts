ab0ce5f01aaa5d90c043a6b52bdc81b930d1432c
Shaunak-AAPT <shaunak.wankhade@finizon.com>
2024-12-14 16:18:23 +0530

    [shaunak]added: FlipCart changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,16 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-// import { provideForms } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(withInterceptorsFromDi()),
     provideRouter(routes),
-    // provideForms()
   ]
 };
-// function provideForms(): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
-//   throw new Error('Function not implemented.');
-// }
+
 
